128f44d11e33431298ad2c4fd110c00c534c49f4
Damien Saavi <damiensaavi@gmail.com>
2021-05-31 19:40:38 -0700

    implemented lazy grid update

--- app.js
+++ app.js
@@ -2,69 +2,101 @@ const app = Vue.createApp({
     data() {
         return {
             grid: [],
-            dimx: 32,
-            dimy: 32,
-            simSpeed: 7,
+            activeCells: {},
+            scope: {},
+            dimx: 64,
+            dimy: 64,
+            simSpeed: 10,
             keepGoing: false,
             mouseDown: false
         }
     },
     methods: {
         updateGrid() {
-            const nextGrid = JSON.parse(JSON.stringify(this.grid))
-            
-            for (let i = 0; i < this.dimy; i++) {
-                for (let j = 0; j < this.dimx; j++) {
-                    
-                    const cell = this.grid[i][j]
-                    let pop = 0;
 
-                    for (let di = i-1; di <= i+1; di++) {
-                        for (let dj = j-1; dj <= j+1; dj++) {
-                            
-                            if ((di < 0 || di >= this.dimy) || (dj < 0 || dj >= this.dimx))
-                                continue
+            this.scope = {}
+            const nextGrid = [...Array(this.dimy)].map(x => Array(this.dimx).fill(0))
+            const nextActiveCells = {}
 
-                            if (!(di == i && dj == j) && this.grid[di][dj] == 1) {
-                                pop++;
-                            }
+            for (cell in this.activeCells) {
+
+                const i = Number(cell.split(',')[0])
+                    , j = Number(cell.split(',')[1])
+
+                for (let di = i - 1; di <= i + 1; di++) {
+                    for (let dj = j - 1; dj <= j + 1; dj++) {
+
+                        if ((di == i && dj == j) || di < 0 || di >= this.dimy || dj < 0 || dj >= this.dimx)
+                            continue
+
+
+                        const neighCell = `${di},${dj}`
+
+                        if (this.scope[neighCell])
+                            this.scope[neighCell]++
+                        else
+                            this.scope[neighCell] = 1
+
+                        if (this.scope[neighCell] > 3 || this.scope[neighCell] < 2) {
+                            this.scope[neighCell]['active'] = false
+                            delete nextActiveCells[neighCell]
+                        } else if (this.scope[neighCell] == 3) {
+                            this.scope[neighCell]['active'] = true
+                            nextActiveCells[neighCell] = 1
+                        } else if (this.activeCells[neighCell]) {
+                            this.scope[neighCell]['active'] = true
+                            nextActiveCells[neighCell] = 1
+                        } else {
+                            this.scope[neighCell]['active'] = false
+                            delete nextActiveCells[neighCell]
                         }
                     }
-
-                    if (pop < 2 || pop > 3)
-                        nextGrid[i][j] = 0
-                    else if (cell == 0 && pop == 3)
-                        nextGrid[i][j] = 1
-                    else
-                        nextGrid[i][j] = cell
                 }
             }
 
+            for (cell in nextActiveCells) {
+
+                const i = Number(cell.split(',')[0])
+                    , j = Number(cell.split(',')[1])
+
+                nextGrid[i][j] = 1
+            }
+
+            this.activeCells = nextActiveCells
             this.grid = nextGrid
 
         },
 
-        toggleCell(i,j) {
-            this.grid[i][j] = (this.grid[i][j]+1)%2
+        toggleCell(i, j) {
+            this.grid[i][j] = (this.grid[i][j] + 1) % 2
+
+            if (this.grid[i][j] == 1)
+                this.activeCells[`${i},${j}`] = 1
+            else
+                delete this.activeCells[`${i},${j}`]
         },
 
-        drawCell(i,j) {
+        drawCell(i, j) {
             if (this.mouseDown)
-                this.toggleCell(i,j)
+                this.toggleCell(i, j)
         },
 
         toggleSim() {
             this.keepGoing = !this.keepGoing
 
-            if(this.keepGoing)
+            if (this.keepGoing)
                 this.runSim()
         },
 
         runSim() {
+            const t0 = performance.now()
             this.updateGrid()
-
+            const t1 = performance.now()
+            
+            console.log(t1-t0)
+            
             if (this.keepGoing)
-                setTimeout(() => this.runSim(), 300 - this.simSpeed*30);
+                setTimeout(() => this.runSim(), 300 - this.simSpeed * 30);
         },
 
         toggleMouse(isDown) {
@@ -72,9 +104,10 @@ const app = Vue.createApp({
         },
 
         clearGrid() {
-            console.log(this.dimx, this.dimy)
             this.keepGoing = false
             this.grid = [...Array(this.dimy)].map(x => Array(this.dimx).fill(0))
+            this.scope = {}
+            this.activeCells = {}
         },
 
         changeX(event) {
@@ -82,7 +115,7 @@ const app = Vue.createApp({
                 event.target.value = 64
             else if (event.target.value < 8)
                 event.target.value = 8
-            
+
             this.dimx = Number(event.target.value)
             this.clearGrid()
         },
@@ -92,7 +125,7 @@ const app = Vue.createApp({
                 event.target.value = 64
             else if (event.target.value < 8)
                 event.target.value = 8
-            
+
             this.dimy = Number(event.target.value)
             this.clearGrid()
         },
@@ -108,7 +141,6 @@ const app = Vue.createApp({
     },
     mounted() {
         this.grid = [...Array(this.dimy)].map(x => Array(this.dimx).fill(0))
-
     },
 })
 
